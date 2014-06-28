4c6273013a445c4fa87d9bc99e0f374e439f4572
Malinda Rajapakse <malinda_rajapakse@hotmail.com>
2014-06-28 01:41:41 +0100

    Test example unit test working with maven surefire

---
+++ src/test/java/com/malsr/TestMavenSureFire.java
@@ -0,0 +1,13 @@
+package com.malsr;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestMavenSureFire {
+
+    @Test
+    public void runsTest() {
+        assertTrue(2 > 0);
+    }
+}
